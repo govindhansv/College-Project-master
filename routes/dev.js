b2b90231c89030bd01081317beee2e6b1a6c5d94
govindhansv <govindhansv333@gmail.com>
2024-03-21 21:12:36 +0530

    updated

--- routes/dev.js
+++ routes/dev.js
@@ -6,7 +6,7 @@ var db = require('../config/connection')
 
 /* GET home page. */
 router.get('/health', async function (req, res, next) {
-    res.status(200).json({"status":"working"});
+    res.send({"status":"working"});
 });
 
 router.get('/db/clean', async function (req, res, next) {
